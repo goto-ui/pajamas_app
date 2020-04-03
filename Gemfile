d3e1a64fdc21587bad9ead36deeace16eb39b509
goto-ui <onion753@gmail.com>
2020-04-03 02:42:38 +0000

    gimeiを導入して試しにユーザーを増やして一覧ページを作りました

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.6.3'
 gem 'bootstrap', '~> 4.4.1'
 gem 'jquery-rails'
 gem 'devise'
+gem 'gimei'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "testユーザー",
+             email: "test@pajama.org",
+             password:              "pajama",
+             password_confirmation: "pajama")
+
+30.times do |n|
+  gimei  = Gimei.name
+  name = gimei.romaji
+  email = "example-#{n+1}@pajamas.org"
+  password = "#{name} + password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
